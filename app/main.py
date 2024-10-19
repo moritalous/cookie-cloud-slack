59da100fb376a03037826983ad6cf5bc14849d03
moritalous <moritalous@gmail.com>
2024-10-19 11:13:01 +0000

    1019版

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
+import datetime
 import json
 import logging
 import os
+import time
+import zoneinfo
 
+import dateparser
 import requests
 from slack_bolt import Ack, App, logger
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from slack_sdk import WebClient
+from slack_sdk.web.slack_response import SlackResponse
 
 # デバッグレベルのログを有効化
 logging.basicConfig(level=logging.DEBUG)
@@ -52,40 +57,116 @@ def message_hello(message, say):
 
 
 @app.command("/command")
-def handle_command_test(ack: Ack, body: dict, client: WebClient):
+def handle_command_test(ack: Ack, body: dict, client: WebClient, logger: logger, say):
 
     ack()
+    logger.info(body)
+    pass
 
-    # views.open という API を呼び出すことでモーダルを開きます
-    client.views_open(
-        trigger_id=body["trigger_id"],
-        view={
-            "type": "modal",
-            # このモーダルに自分で付けられる ID で、次に説明する @app.view リスナーはこの文字列を指定します
-            "callback_id": "modal-id",
-            # これは省略できないため、必ず適切なテキストを指定してください
-            "title": {"type": "plain_text", "text": "テストモーダル"},
-            "submit": {"type": "plain_text", "text": "送信"},
-            "close": {"type": "plain_text", "text": "閉じる"},
-            "blocks": [
-                {
-                    "type": "input",
-                    # block_id / action_id を指定しない場合 Slack がランダムに指定します
-                    # この例のように明に指定することで、@app.view リスナー側での入力内容の取得で
-                    # ブロックの順序に依存しないようにすることをおすすめします
-                    "block_id": "question-block",
-                    # ブロックエレメントの一覧は https://api.slack.com/reference/block-kit/block-elements
-                    # Works with block types で Input がないものは input ブロックに含めることはできません
-                    "element": {
-                        "type": "plain_text_input",
-                        "action_id": "input-element",
-                    },
-                    # これはモーダル上での見た目を調整するものです
-                    # 同様に placeholder を指定することも可能です
-                    "label": {"type": "plain_text", "text": "質問"},
-                }
-            ],
+
+@app.command("/yaruki")
+def handle_command_yaruki(ack: Ack, body: dict, client: WebClient, logger: logger, say):
+
+    ack()
+    logger.info(body)
+
+    text = body["text"]
+
+    channel_id = body["channel_id"]
+    history = client.conversations_history(channel=channel_id, limit=20)
+    messages = history["messages"]
+
+    messages = sorted(messages, key=lambda x: x["ts"], reverse=False)
+
+    for m in messages:
+        if "blocks" in m:
+            del m["blocks"]
+
+    dt_now = datetime.datetime.now(zoneinfo.ZoneInfo("Asia/Tokyo"))
+    now = dt_now.strftime("%Y年%m月%d日 %H:%M:%S")
+
+    response = requests.post(
+        DIFY_API_APP_URL,
+        headers={"Authorization": f"Bearer {DIFY_API_APP4_TOKEN}"},
+        json={
+            "inputs": {
+                "chat_history": json.dumps(messages, ensure_ascii=False),
+                "today": now,
+                "prompt": text,
+            },
+            "response_mode": "blocking",
+            "user": DIFY_API_TOKEN_USER,
         },
+    )
+
+    response_json = response.json()
+    output = response_json["data"]["outputs"]["text"]
+
+    client.chat_postEphemeral(
+        channel=body["channel_id"],
+        user=body["user_id"],
+        text=output,
+    )
+
+
+@app.command("/yaruki_reminder")
+def handle_command_yaruki_reminder(
+    ack: Ack, body: dict, client: WebClient, logger: logger, say
+):
+    ack()
+    logger.info(body)
+
+    text = body["text"]
+
+    channel_id = body["channel_id"]
+    history = client.conversations_history(channel=channel_id, limit=20)
+    messages = history["messages"]
+
+    messages = sorted(messages, key=lambda x: x["ts"], reverse=False)
+
+    for m in messages:
+        if "blocks" in m:
+            del m["blocks"]
+
+    dt_now = datetime.datetime.now(zoneinfo.ZoneInfo("Asia/Tokyo"))
+    now = dt_now.strftime("%Y年%m月%d日 %H:%M:%S")
+
+    response = requests.post(
+        DIFY_API_APP_URL,
+        headers={"Authorization": f"Bearer {DIFY_API_APP3_TOKEN}"},
+        json={
+            "inputs": {
+                "chat_history": json.dumps(messages, ensure_ascii=False),
+                "date": now,
+            },
+            "response_mode": "blocking",
+            "user": DIFY_API_TOKEN_USER,
+        },
+    )
+
+    response_json = response.json()
+    task_list = response_json["data"]["outputs"]["task_list"]
+
+    for task in task_list:
+        term = task["term"]
+        description = task["description"]
+        status = task["status"]
+
+        parsed_date = dateparser.parse(term.split("～")[0])
+
+        if parsed_date:
+
+            client.chat_postEphemeral(
+                channel=body["channel_id"],
+                user=body["user_id"],
+                text=f"{parsed_date.strftime('%Y年%m月%d日 %H:%M:%S')}にリマインダーをセットします。\n\n{description}\n\n頑張ってね！！（ここ生成する）",
+            )
+
+    client.chat_scheduleMessage(
+        channel=body["channel_id"],
+        user=body["user_id"],
+        text="タスクは終わったかな？？（スケジュールメッセージ",
+        post_at=int(time.time()) + 60,
     )
 
 
@@ -107,6 +188,22 @@ DIFY_API_APP1_TOKEN = os.environ.get("DIFY_API_APP1_TOKEN")
 DIFY_API_APP2_TOKEN = os.environ.get("DIFY_API_APP2_TOKEN")
 DIFY_API_TOKEN_USER = os.environ.get("DIFY_API_TOKEN_USER")
 DIFY_API_APP_URL = os.environ.get("DIFY_API_APP_URL")
+
+### ショートカットアプリ２
+APP2_SHORTCUT_ID = "task_manager_001"
+DIFY_API_APP3_TOKEN = os.environ.get("DIFY_API_APP3_TOKEN")
+APP2_CALLBACK_ID = "APP2_CALLBACK_ID"
+APP2_MODAL1_BLOCK1_ID = "APP2_MODAL1_BLOCK1_ID"
+APP2_MODAL1_BLOCK1_ACTIONID = "APP2_MODAL1_BLOCK1_ACTIONID"
+APP2_MODAL1_BLOCK2_ID = "APP2_MODAL1_BLOCK2_ID"
+APP2_MODAL1_BLOCK2_ACTIONID = "APP2_MODAL1_BLOCK2_ACTIONID"
+APP2_MODAL1_BLOCK3_ID = "APP2_MODAL1_BLOCK3_ID"
+APP2_MODAL1_BLOCK3_ACTIONID = "APP2_MODAL1_BLOCK3_ACTIONID"
+APP2_MODAL1_BLOCK4_ID = "APP2_MODAL1_BLOCK4_ID"
+APP2_MODAL1_BLOCK4_ACTIONID = "APP2_MODAL1_BLOCK4_ACTIONID"
+
+### コマンドアプリ
+DIFY_API_APP4_TOKEN = os.environ.get("DIFY_API_APP4_TOKEN")
 
 
 def app1_create_view(
@@ -166,7 +263,7 @@ def app1_create_view(
             "block_id": APP1_MODAL1_BLOCK4_ID,
             "text": {"type": "mrkdwn", "text": "送信先を選択"},
             "accessory": {
-                "type": "users_select",
+                "type": "channels_select",
                 "action_id": APP1_MODAL1_BLOCK4_ACTIONID,
                 "placeholder": {
                     "type": "plain_text",
@@ -207,11 +304,165 @@ def app1_create_view(
     return {
         "type": "modal",
         "callback_id": callback_id,
+        "title": {"type": "plain_text", "text": "タスク管理くん"},
+        "submit": {"type": "plain_text", "text": "送信"},
+        "close": {"type": "plain_text", "text": "閉じる"},
+        "blocks": blocks,
+    }
+
+
+# チャンネル名 : channel_id
+channel_list = {"user1-bot": "C07S4DSDMBQ"}
+
+
+def app2_create_view(callback_id: str, task_list: list = None):
+
+    blocks = []
+
+    block_1 = {
+        "type": "actions",
+        "block_id": APP2_MODAL1_BLOCK1_ID,
+        "elements": [
+            {
+                "type": "button",
+                "text": {
+                    "type": "plain_text",
+                    "text": f"タスク取得",
+                },
+                "value": "click_me_123",
+                "action_id": APP2_MODAL1_BLOCK1_ACTIONID,
+            }
+        ],
+    }
+
+    blocks.append(block_1)
+
+    if task_list:
+
+        task_str = [
+            f"{task['term']} {task['description']} {task['status']}"
+            for task in task_list
+        ]
+        task_str = "\n".join(task_str)
+
+        block_2 = {
+            "type": "input",
+            "block_id": APP2_MODAL1_BLOCK2_ID,
+            "element": {
+                "type": "plain_text_input",
+                "action_id": APP2_MODAL1_BLOCK2_ACTIONID,
+                "multiline": True,
+                "initial_value": task_str,
+            },
+            "label": {"type": "plain_text", "text": "タスク"},
+        }
+
+        blocks.append(block_2)
+
+        block_3 = {
+            "type": "input",
+            "block_id": APP2_MODAL1_BLOCK3_ID,
+            "element": {
+                "type": "plain_text_input",
+                "action_id": APP2_MODAL1_BLOCK3_ACTIONID,
+                "multiline": True,
+            },
+            "label": {"type": "plain_text", "text": "変更ある？？"},
+        }
+
+        blocks.append(block_3)
+
+        block_4 = {
+            "type": "actions",
+            "block_id": APP2_MODAL1_BLOCK4_ID,
+            "elements": [
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": f"お願い！",
+                    },
+                    "value": "click_me_123",
+                    "action_id": APP2_MODAL1_BLOCK4_ACTIONID,
+                }
+            ],
+        }
+
+        blocks.append(block_4)
+
+    return {
+        "type": "modal",
+        "callback_id": callback_id,
         "title": {"type": "plain_text", "text": "モーダル"},
         "submit": {"type": "plain_text", "text": "送信"},
         "close": {"type": "plain_text", "text": "閉じる"},
         "blocks": blocks,
     }
+
+
+@app.shortcut(APP2_SHORTCUT_ID)
+def handle_shortcuts_app2(ack: Ack, body: dict, client: WebClient, logger: logger):
+
+    logger.info(body)
+    ack()
+
+    # views.open という API を呼び出すことでモーダルを開きます
+    client.views_open(
+        trigger_id=body["trigger_id"],
+        view=app2_create_view(APP2_CALLBACK_ID),
+    )
+
+
+@app.action(APP2_MODAL1_BLOCK1_ACTIONID)
+def handle_action_app2_modal1_block1(
+    ack: Ack, body: dict, client: WebClient, logger: logger
+):
+    ack()
+    logger.info(body)
+
+    channel_id = channel_list["user1-bot"]
+    history = client.conversations_history(channel=channel_id, limit=100)
+    messages = history["messages"]
+
+    messages = sorted(messages, key=lambda x: x["ts"], reverse=False)
+
+    for m in messages:
+        if "blocks" in m:
+            del m["blocks"]
+
+    # logger.info(json.dumps(messages, indent=2, ensure_ascii=False))
+
+    dt_now = datetime.datetime.now(zoneinfo.ZoneInfo("Asia/Tokyo"))
+    now = dt_now.strftime("%Y年%m月%d日 %H:%M:%S")
+
+    response = requests.post(
+        DIFY_API_APP_URL,
+        headers={"Authorization": f"Bearer {DIFY_API_APP3_TOKEN}"},
+        json={
+            "inputs": {
+                "chat_history": json.dumps(messages, ensure_ascii=False),
+                "date": now,
+            },
+            "response_mode": "blocking",
+            "user": DIFY_API_TOKEN_USER,
+        },
+    )
+
+    response_json = response.json()
+    task_list = response_json["data"]["outputs"]["task_list"]
+
+    logger.info(json.dumps(task_list, indent=2, ensure_ascii=False))
+
+    client.views_update(
+        view_id=body.get("view").get("id"),
+        hash=body.get("view").get("hash"),
+        view=app2_create_view(
+            callback_id=APP2_CALLBACK_ID,
+            task_list=task_list,
+        ),
+    )
+
+    pass
 
 
 @app.shortcut(APP1_SHORTCUT_ID)
@@ -325,10 +576,7 @@ def handle_view_app1_callback(ack: Ack, body: dict, logger: logging.Logger):
     response = requests.post(
         DIFY_API_APP_URL,
         headers={"Authorization": f"Bearer {DIFY_API_APP2_TOKEN}"},
-        json={
-            "user": DIFY_API_TOKEN_USER,
-            "inputs": {}
-        },
+        json={"user": DIFY_API_TOKEN_USER, "inputs": {}},
     )
 
     response_json = response.json()
